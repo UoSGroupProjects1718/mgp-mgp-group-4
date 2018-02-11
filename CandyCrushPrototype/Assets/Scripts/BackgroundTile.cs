447ad277f15244589b23cb2a4ee429459c97e962
JimmyVictor <rhukz@yahoo.com>
2018-02-11 16:04:21 +0000

    First Commit
    Created the layout of a candy crush-like puzzle, where the dots are arranged randomly each time you start a new game.

---
+++ CandyCrushPrototype/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour {
+
+    public GameObject[] dots;
+
+	// Use this for initialization
+	void Start () {
+        Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Initialize()
+    {
+        int dotToUse = Random.Range(0, dots.Length);
+        GameObject dot = Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
+    }
+}
